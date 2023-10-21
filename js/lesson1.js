2a804e62d136a93f32f265714246a8ac797f3e76
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2023-10-21 15:08:29 +0300

    task1

---
+++ js/lesson1.js
@@ -0,0 +1,8 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+
+const name = "Iryna";
+const age = 47;
+const hobby = "JS";
+let massege = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
+console.log(massege);
