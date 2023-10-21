1c300d267dc640ae047e0c0a35e0803d05cd9cf7
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2023-10-21 16:10:24 +0300

    task3

--- js/lesson1.js
+++ js/lesson1.js
@@ -11,11 +11,23 @@
 // ЗАДАЧА 2
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
 
-const userName = prompt("Введіть своє ім'я");
-const userMail = prompt("Введіть Вашу пошту");
-const userPhone = prompt("Введіть Ваш номер телефону");
+// const userName = prompt("Введіть своє ім'я");
+// const userMail = prompt("Введіть Вашу пошту");
+// const userPhone = prompt("Введіть Ваш номер телефону");
 
-let message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ  ${userMail} ПОЧТУ І ${userPhone} ТЕЛЕФОН`
+// let message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ  ${userMail} ПОЧТУ І ${userPhone} ТЕЛЕФОН`
 
-console.log(message);
-alert(message);
+// console.log(message);
+// alert(message);
+
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+
+const mobile = Number(prompt("Введіть ціну телефона"));
+const simCard = Number(prompt("Введіть ціну sim-картки"));
+const caseMobile = +prompt("Введіть ціну чохла на телефон");
+let sum = mobile + simCard + caseMobile;
+
+console.log(sum);
+alert(sum);
