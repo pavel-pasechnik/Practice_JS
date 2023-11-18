1b1b5551258a5432179500f5feffa9e17ad20eb1
Pavel <Pavel.Pasechnik@gmail.com>
2023-11-18 16:11:28 +0200

    task 3

--- js/lesson3.js
+++ js/lesson3.js
@@ -20,17 +20,112 @@
 // showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
 // makeProduct("Холодильник", 10000, logger);
 
-function makeProduct(name = "", price = 0, showProduct) {
-  const product = {
-    name,
-    price,
-    id: Date.now(),
-  };
-  showProduct(product);
-}
+// function makeProduct(name = "", price = 0, showProduct) {
+//   const product = {
+//     name,
+//     price,
+//     id: Date.now(),
+//   };
+//   showProduct(product);
+// }
 
-function showProduct(product) {
-  console.log(product);
-}
+// function showProduct(product) {
+//   console.log(product);
+// }
 
-makeProduct("Холодильник", 10000, showProduct);
+// makeProduct("Холодильник", 10000, showProduct);
+
+// Задача 3
+
+const scientists = [
+  {
+    name: "Albert",
+    surname: "Einstein",
+    born: 1879,
+    dead: 1955,
+    id: 1,
+  },
+  {
+    name: "Isaac",
+    surname: "Newton",
+    born: 1643,
+    dead: 1727,
+    id: 2,
+  },
+  {
+    name: "Galileo",
+    surname: "Galilei",
+    born: 1564,
+    dead: 1642,
+    id: 3,
+  },
+  {
+    name: "Marie",
+    surname: "Curie",
+    born: 1867,
+    dead: 1934,
+    id: 4,
+  },
+  {
+    name: "Johannes",
+    surname: "Kepler",
+    born: 1571,
+    dead: 1630,
+    id: 5,
+  },
+  {
+    name: "Nicolaus",
+    surname: "Copernicus",
+    born: 1473,
+    dead: 1543,
+    id: 6,
+  },
+  {
+    name: "Max",
+    surname: "Planck",
+    born: 1858,
+    dead: 1947,
+    id: 7,
+  },
+  {
+    name: "Katherine",
+    surname: "Blodgett",
+    born: 1898,
+    dead: 1979,
+    id: 8,
+  },
+  {
+    name: "Ada",
+    surname: "Lovelace",
+    born: 1815,
+    dead: 1852,
+    id: 9,
+  },
+  {
+    name: "Lise",
+    surname: "Meitner",
+    born: 1878,
+    dead: 1968,
+    id: 11,
+  },
+  {
+    name: "Sarah E.",
+    surname: "Goode",
+    born: 1855,
+    dead: 1905,
+    id: 10,
+  },
+  {
+    name: "Hanna",
+    surname: "Hammarström",
+    born: 1829,
+    dead: 1909,
+    id: 12,
+  },
+];
+
+// 1)получити масив вчених які народились у 19ст;
+
+const born19Centenary = scientists.filter(({ born }) => born >= 1800 && born < 1900);
+
+console.log(born19Centenary);
