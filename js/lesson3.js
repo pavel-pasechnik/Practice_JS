b0cc685e70be2d1b47c96de3833edae90fb1d36e
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-01-26 13:07:45 +0200

    add taskes

--- js/lesson3.js
+++ js/lesson3.js
@@ -137,5 +137,118 @@ const scientists = [
 
 // 3) відсортувати за алфавітом;
 
-const nameAlfabet = [...scientists].sort((a, b) => (a.name[0] > b.name[0] ? -1 : 1));
-console.log(nameAlfabet);
+// const nameAlfabet = [...scientists].sort((a, b) => (a.name[0] > b.name[0] ? -1 : 1));
+// console.log(nameAlfabet);
+
+// const nameAlphfabet = [...scientists].sort((a, b) => a.name.localeCompare(b.name));
+// console.log(nameAlphfabet);
+
+// 4)відсортувати по кількості прожитих років;
+
+// const sorted = scientists.toSorted((a, b) => (a.dead - a.born) - (b.dead - b.born));
+
+// console.log(sorted);
+
+// 5)відфільтрувати, хто народився в 15,16,17ст;
+
+// const filter = scientists.filter((year) => year.born >= 1400 && year.born < 1700);
+
+// console.log(filter);
+
+// 6)знайти рік народження Albert Einstein;
+
+// const born = scientists.find(
+//   (element) => element.name === "Albert" && element.surname === "Einstein"
+// );
+
+// console.log(born.born);
+
+// ! ЗАДАЧА 4
+// Сортування примітивів.
+// Виконати сортування масиву цін зі спадання та зростання.
+// const prices = [2, 14, 1, 37, 26, 8];
+
+// let sortNumber = [2, 14, 1, 37, 26, 8].sort((a, b) => a - b);
+// console.log(sortNumber);
+
+// const prices = [2, 14, 1, 37, 26, 8];
+// let sortNumber = prices.toSorted((a, b) => a - b);
+// console.log(sortNumber);
+
+// const prices = [2, 14, 1, 37, 26, 8];
+// let sortNumber = prices.toSorted((a, b) => b - a);
+// console.log(sortNumber);
+
+//
+
+// ! ЗАДАЧА 5
+// Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
+// const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
+
+// ! ЗАДАЧА 6
+// Зібрати в allTopics масив усіх предметів всіх курсів використовуючи flatMap.
+// Використовуючи Array.prototype.filter виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи.
+
+// const courses = [
+//   {
+//     name: "Basic HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub Desktop", "GitHub"],
+//   },
+//   {
+//     name: "Intermediate HTML+CSS",
+//     topics: ["VSCode", "Terminal", "Git", "GitHub", "HTML", "CSS"],
+//   },
+//   {
+//     name: "Basic JavaScript",
+//     topics: [
+//       "VSCode",
+//       "Type system",
+//       "Loops",
+//       "Functions",
+//       "Conditions",
+//       "Classes",
+//       "DOM",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+//   {
+//     name: "Intermediate JavaScript",
+//     topics: [
+//       "VSCode",
+//       "NPM",
+//       "Bundlers",
+//       "Transpiling",
+//       "Promises",
+//       "AJAX",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+// ]
+
+// ! ЗАДАЧА 7
+// Напишіть клас User для створення користувача з наступними властивостями:
+// username - імя, рядок
+// age - вік, число
+// numberOfPosts - кількість постів, число
+// Клас очікує один параметр - об'єкт налаштувань з однойменними властивостями.
+// Добавте метод getInfo(), який повертає рядок:
+// User ${ імя } is ${ вік } years old and has ${ кількість постів } posts.
+
+// ! ЗАДАЧА 8
+// Напиши клас Storage який створює об'єкти для керування складом товарів.
+// При виклику отримуватиме один аргумент - початковий масив товарів, і записуватиме його у властивість items.
+// Добавте методи класу:
+
+// getItems() - повертає масив товарів.
+// addItem(item) - отримує новий товар та додає його до поточних.
+// removeItem(item) - отримає товар і, якщо він є, видаляє його з поточних.
+
+// const items = new Storage(["Виноград", "Банани", "Кокоси", "Ківі"]);
+// items.addItem("Манго");
+// items.removeItem("Ківі")
+
+// ! ЗАДАЧА 9
+// Напиши клас Client який створює об'єкт із властивостями login і email.
+// Оголоси приватні властивості #login і #email, доступ до яких зроби через геттер та сеттер login і email.
